0c0a75c13bdaa7af5eb9a192e03f9daa0d76006c
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2025-11-13 19:17:13 +0000

    Add gravity simulation for n-body problem
    This file contains a numerical simulation of gravitationally interacting objects using the leapfrog numerical scheme. It includes functions for initialization, updating positions, and calculating energies.

---
+++ n-body/gravity.js
@@ -0,0 +1,273 @@
+// numerical simulation of the initial value problem of gravitationally interacting objects of the same mass
+// integrated in time using leapfrog (DKD) numerical scheme
+// Alex Pedcenko, 2020
+var n = 6;
+var G = 1, i, j, theta = [], t=0, dx, dy, r_cube, v02 = [], X = [], Y = [], x = [], y = [], KE = [0], PE = [0], E = [];
+var vx = [], vy = [], ax = [], ay = [], pe = [], ke = [], lines = [], coeff;
+var k = 0; //iterations counter
+var dt, dt2, t;
+
+var T = []; // time array
+var m = new Array(n);
+var data = [];
+
+var R = 1; //initial orbital radius
+var v0 = []; // initial orbital speeds
+
+//console.log('n='+oFormObject.elements["N"].value);
+dt=0.01;
+init();
+
+
+requestAnimationFrame(update);
+
+function update () { // update plot
+	compute();
+
+if( (k % 2) == 0) {
+	Plotly.redraw('graph');
+	Plotly.redraw('energy_plot');
+}
+	//setTimeout(function(){ requestAnimationFrame(update); }, 1);
+	requestAnimationFrame(update);
+	
+if ( E[k] > 0) {
+Plotly.relayout( "energy_plot", {
+    yaxis: {autorange: true}
+});
+}
+
+if(Math.max.apply(null, x.map(Math.abs)) > 2 ) {
+Plotly.relayout( "graph", {
+    xaxis: {constraint: 'domain'},
+    yaxis: {scaleanchor: 'x'}
+});
+}
+
+	if(Math.max.apply(null, x.map(Math.abs))>10 || Math.max.apply(null, y.map(Math.abs))>10) {
+	init();
+	}
+}
+
+
+
+
+
+///////////////////////////////////////////////////
+function init () {
+
+n = document.getElementById('N').value;
+dt = document.getElementById('timestep').value;
+coeff = document.getElementById('coeff').value;
+dt2=dt/2;
+
+//console.log(dt);
+
+x = [], y = [], KE = 0, PE = 0, E = [];
+vx = [], vy = [], ax = [], ay = [], pe = [], ke = [], lines = [], X = [], Y = [];
+KE = [0], PE = [0];
+k = 0;
+
+T = []; // time array
+m = new Array(n);
+X = new Array(n);
+Y = new Array(n);
+
+for (var i = 0; i < n; i++) { 
+    X[i] = []; 
+    Y[i] = [];
+}
+
+
+
+data = [];
+
+R = 1; //initial orbital radius
+v0 = []; // initial orbital speeds
+
+
+for (i = 0; i < n; i++) {
+     m[i] = 1;
+     theta[i] = i * 2 * Math.PI/n;
+     x[i]=R*Math.cos(theta[i]);
+     y[i]=R*Math.sin(theta[i]);
+     X[i][k]=x[i];
+     Y[i][k]=y[i];
+
+}
+energy();
+
+for (i = 0; i < n; i++) {	
+	
+     v0[i] = Math.sqrt(Math.abs(pe[i])/2) * coeff; // Keplerian speeds for each mass
+     vx[i] = -v0[i] * Math.sin(theta[i]);
+     vy[i] = v0[i] * Math.cos(theta[i]);
+}
+
+init_plot();
+init_plot2();
+
+
+
+}
+
+/////////////////////////////////////////////////////////
+
+
+function init_plot() {
+Plotly.purge('graph');
+var trace = {
+    x: x,
+    y: y,
+    type: 'scatter',
+    mode: 'markers',
+    marker: { size: 12 }
+};
+
+data.push(trace);
+
+for (i = 0; i < n; i++) {
+var lines = {
+    x: X[i],
+    y: Y[i],
+    mode: 'lines',
+    line: {width: 1}
+};
+
+data.push(lines);
+}
+
+
+var layout = {
+	xaxis: { range: [ -2, 2 ] },
+	yaxis: { range: [-2, 2] },
+	showlegend: false,
+	height: 600,
+	width: 600
+};
+
+
+Plotly.newPlot('graph', data, layout, {scrollZoom: true});
+}
+//////////////////////////////////////////////////////////
+function init_plot2() {
+Plotly.purge('energy_plot');
+var trace2 = {
+    x: T,
+    y: KE,
+    type: 'scatter',
+    name: 'KE'
+};
+
+var trace3 = {
+    x: T,
+    y: PE,
+    type: 'scatter',
+    name: 'PE'
+};
+
+var trace4 = {
+    x: T,
+    y: E,
+    type: 'scatter',
+    name: 'KE+PE'
+};
+
+
+data2 = [ trace2, trace3, trace4 ];
+
+var layout2 = {
+	title:'Total Energy',
+	xaxis: { autorange: true },
+	yaxis: { autorange: true },
+	//yaxis: { range: [-240, -220] },
+	showlegend: true,
+	height: 300,
+	width: 600
+};
+
+
+Plotly.newPlot('energy_plot', data2, layout2, {scrollZoom: true});
+
+}
+//////////////////////////////////////////////////////////
+
+function compute () {
+
+k += 1;
+t = k * dt;
+// Lepfrog 
+
+// Leapfrog 1st half-step
+for (i = 0; i < n; i++) {
+	x[i] = x[i] + vx[i]*dt2;
+	y[i] = y[i] + vy[i]*dt2;
+}
+
+// forces
+for (i = 0; i < n; i++) {
+	ax[i]=0;
+	ay[i]=0;
+for (j = 0; j < n; j++) {
+      if (j !== i) {
+         dx = x[i] - x[j];
+         dy = y[i] - y[j];
+	 r_cube = Math.pow((dx*dx + dy*dy),(3/2));
+         ax[i] = ax[i] - G*m[j]*dx/r_cube; 
+         ay[i] = ay[i] - G*m[j]*dy/r_cube; 
+      		}
+	}
+}
+
+// Leapfrog 2nd half-step
+for (var i = 0; i < n; i++) {
+	vx[i] = vx[i] + ax[i]*dt;
+	vy[i] = vy[i] + ay[i]*dt;
+
+	x[i] = x[i] + vx[i]*dt2;
+	y[i] = y[i] + vy[i]*dt2;
+
+	X[i][k] = x[i];
+	Y[i][k] = y[i];
+}
+
+energy(); // calculate energies
+
+T[k]=t;
+//console.log(t);
+document.getElementById("E").innerHTML = 'Total Energy: ' + E[k];
+//document.getElementById("time").innerHTML = t;
+
+} 
+
+
+//////////////////////////////////////////
+
+function energy () { // energy
+// P.E.
+//PE = 0;
+//KE = 0;
+for (i = 0; i < n; i++) {
+pe[i] = 0;
+ke[i] = 0;
+	for (j = 0; j < n; j++) {
+		if (j !== i) {
+			dx = x[i] - x[j];
+		        dy = y[i] - y[j];
+			r = Math.pow(dx*dx + dy*dy, 0.5);
+			pe[i] = pe[i] - G*m[i]*m[j]/r;
+			//console.log(pe[i]);
+		}
+	}
+ke[i] = m[i]*(vx[i]*vx[i]+vy[i]*vy[i]);
+//console.log(ke[i]);
+}
+
+// K.E.
+
+KE[k] = ke.reduce((a, b) => a + b, 0);
+PE[k] = pe.reduce((a, b) => a + b, 0);
+
+E[k]=KE[k]+PE[k];	
+}
+///////////////////////////////////////////
