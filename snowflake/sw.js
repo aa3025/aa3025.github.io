3d0debd208ac1f43f511935008d919c41998fe6f
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2026-02-19 13:22:16 +0000

    Add files via upload

---
+++ snowflake/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'fractal-explorer-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js'
+];
+
+// Install Event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Event (Network falling back to cache)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
+
+// Activation Event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
